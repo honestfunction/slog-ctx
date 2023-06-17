b048d014bbb6c96e51e35ba9a22b47ef403fdf23
honestfunction <honestfunction@gmail.com>
2023-06-17 15:16:06 +0800

    Add simple test case

--- tests/slogctx_test.go
+++ tests/slogctx_test.go
@@ -36,3 +36,65 @@ func TestDupKey(t *testing.T) {
 		t.Error("not the last value:", lastVal, jsonKeyValue["key"])
 	}
 }
+
+func TestSimpleLayers(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	buf := bytes.NewBuffer([]byte{})
+	reader := bufio.NewReader(buf)
+
+	handler := slogctx.Setup(slog.NewJSONHandler(buf, nil))
+	slog.SetDefault(slog.New(handler))
+
+	commonKey := "category"
+
+	rootCtx := slogctx.With(ctx, commonKey, "root")
+
+	slog.InfoCtx(rootCtx, "from root")
+
+	line, _, _ := reader.ReadLine()
+	jsonKeyValue := map[string]string{}
+	json.Unmarshal(line, &jsonKeyValue)
+	if jsonKeyValue[commonKey] != "root" {
+		t.Error("not the expected value: root", jsonKeyValue[commonKey])
+	}
+
+	func(ctxIn context.Context) {
+		ctx := slogctx.With(ctxIn, commonKey, "fun1")
+		ctx = slogctx.With(ctx, "fun1", "fun1")
+		slog.InfoCtx(ctx, "from fun1")
+
+		line, _, _ := reader.ReadLine()
+		jsonKeyValue := map[string]string{}
+		json.Unmarshal(line, &jsonKeyValue)
+		if jsonKeyValue[commonKey] != "fun1" {
+			t.Error("not the expected value: fun1", jsonKeyValue[commonKey])
+		}
+		if jsonKeyValue["fun1"] != "fun1" {
+			t.Error("not the expected value: fun1", jsonKeyValue["fun1"])
+		}
+
+	}(rootCtx)
+
+	func(ctxIn context.Context) {
+		ctx := slogctx.With(ctxIn, commonKey, "fun2")
+		ctx = slogctx.With(ctx, "fun2", "fun2")
+		slog.InfoCtx(ctx, "from fun2")
+
+		line, _, _ := reader.ReadLine()
+		jsonKeyValue := map[string]string{}
+		json.Unmarshal(line, &jsonKeyValue)
+		if jsonKeyValue[commonKey] != "fun2" {
+			t.Error("not the expected value: fun2", jsonKeyValue[commonKey])
+		}
+		if jsonKeyValue["fun2"] != "fun2" {
+			t.Error("not the expected value: fun2", jsonKeyValue["fun2"])
+		}
+
+		if jsonKeyValue["fun1"] != "" {
+			t.Error("not the expected value: {}", jsonKeyValue["fun1"])
+		}
+
+	}(rootCtx)
+}
